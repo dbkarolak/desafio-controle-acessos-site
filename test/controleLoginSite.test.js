b8a0b1313ac1823f1760009a1889ad3f52af727e
dbkarolak <deniskarolak@gmail.com>
2026-05-08 20:12:55 -0300

    Adicionado cenário de teste para autenticação com senha expirada.

--- test/controleLoginSite.test.js
+++ test/controleLoginSite.test.js
@@ -13,4 +13,16 @@ describe('Testes do Controle de Logins do Site', function () {
         // Assert
         assert.equal(retornoDaFuncao, 'Login realizado com sucesso.')
     });
+
+    it('Validar que é requisitado a renovação das credenciais ao tentar autenticar um usuário com email válido e senha válida porém expirada.', function (){
+        // Arrange
+        const emailValido = "julio@sitedojulio.com.br";
+        const senhaValidaExpirada = "SenhaJ";
+
+        // Act
+        const retornoDaFuncao = realizarLogin(emailValido, senhaValidaExpirada);
+
+        // Assert
+        assert.equal(retornoDaFuncao,'Renove suas credenciais.');
+    });
 })
